cc72842fba18fc7ffacef99c961c110fa98b035a
Sayali Naik <21sayalinaik@gmail.com>
2018-12-23 22:54:52 -0800

    Add files via upload

---
+++ Project2-Dec22/src/Application.java
@@ -0,0 +1,20 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt=3;
+		
+		String text="Hello";
+		String blank=" ";
+		String name ="Sayali";
+		String greeting= text+blank+name;
+		System.out.println(greeting);
+		
+		System.out.println("Hello"+" "+"Sayali");
+		
+		System.out.println("My integer is"+" "+myInt);
+		
+		double myDouble= 6.39;
+		System.out.println("My number is "+myDouble+".");
+	}
+}
+
