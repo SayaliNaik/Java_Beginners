8afb40c5c2f40ef1b59ea3100c3d807a08c19a38
Sayali Naik <21sayalinaik@gmail.com>
2019-03-20 22:01:47 -0700

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,22 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Sayali";
+		System.out.println(name);
+		
+		int myNum = 15;
+		System.out.println(myNum);
+		
+		float myFloatNum = 3.9f;
+		char myLetter= 'S';
+		boolean myBool = true;
+		
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		
+	}
+
+}
